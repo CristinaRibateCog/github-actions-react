6d281df4de1cd858e9c0becd714ffee9c4158777
CristinaRibateCog <Cristina.DelAlamo@cognizant.com>
2023-05-02 11:46:19 +0200

    fix: changelog

--- release.config.js
+++ release.config.js
@@ -8,8 +8,8 @@ module.exports = {
       "@semantic-release/github",
       {
         assets: [
-          { path: "build.zip", label: "Build" },
-          { path: "coverage.zip", label: "Coverage" }
+          { path: "build/build.zip", label: "Build" },
+          { path: "coverage/coverage.zip", label: "Coverage" }
         ]
       }
     ],
