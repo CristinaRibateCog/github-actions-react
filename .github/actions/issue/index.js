b425c690965ce25b81805a3cdd2e74e3aff1dcee
CristinaRibateCog <Cristina.DelAlamo@cognizant.com>
2023-05-03 11:23:48 +0200

    fix: create issue 5

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -15,7 +15,7 @@ async function run() {
     console.log("assignees", assignees, assignees.split("\n"));
     console.log("owner: ", github.context.repo.owner, "github.context.repository.owner.name");
     console.log("repo: ", github.context.repo.repo, "github.context.repository.name");
-    console.log("github: ", github.context.repository);
+    console.log("github: ", github.context.repo);
 
     const response = await octokit.rest.issues.create({
       owner: github.context.repo.owner,
